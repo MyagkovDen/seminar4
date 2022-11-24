bb83d32da482a5d94a0a747d6e8118716b35b804
Denis Myagkov <tomich-84@mail.ru>
2022-11-24 23:53:59 +0300

    add s04sum27

---
+++ s04sum27/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 782 -> 10
+// 9012 -> 12
+
+
+int numberA = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int sum = SumNumerals(numberA);
+Console.WriteLine($"{numberA} -> {sum}");
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int SumNumerals(int number)
+{
+    sum = 0;
+    if (number < 0)
+        number = number * (-1);
+
+    while (number > 0)
+    {
+        int numeral = number % 10;
+        sum = sum + numeral;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+
+        else
+            Console.WriteLine(errorMessage);
+    }
+}
