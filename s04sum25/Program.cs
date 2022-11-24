472842c618219dce893dd6a0c441d3137cecca6a
Denis Myagkov <tomich-84@mail.ru>
2022-11-24 23:16:01 +0300

    add s04sum25

---
+++ s04sum25/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int numberA = GetNumberFromUser("Введите число A: ", "Ошибка ввода!");
+int numberB = GetNumberFromUser("Введите число B: ", "Ошибка ввода!");
+int result = RaiseToPower(numberA, numberB);
+Console.WriteLine($"{numberA}, {numberB} -> {result}");
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+int RaiseToPower(int number, int power)
+{
+    int res = 1;
+    for (int i = 0; i < power; i++)
+    {
+        res = res * number;
+    }
+    return res;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+
+        else
+            Console.WriteLine(errorMessage);
+    }
+}
